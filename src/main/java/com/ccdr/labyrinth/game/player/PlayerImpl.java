5e2b34be3e8bf71172968a3a763971ba11b7c347
Lorenzo Carletti <lorenzo.carletti3@studio.unibo.it>
2024-02-09 19:02:44 +0100

    Added controls on some methods in PlayerImpl

--- src/main/java/com/ccdr/labyrinth/game/player/PlayerImpl.java
+++ src/main/java/com/ccdr/labyrinth/game/player/PlayerImpl.java
@@ -4,38 +4,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.ccdr.labyrinth.Material;
+import com.ccdr.labyrinth.game.GameConfig;
 import com.ccdr.labyrinth.game.loader.Coordinate;
 
 public class PlayerImpl implements Player{
 
     private final Map<Material, Integer> playerInventory = new HashMap<>();
     private Coordinate coord;
+    private GameConfig gameconfig = new GameConfig();
     private int points = 0;
 
     public PlayerImpl() {
         for (var material : Material.values()) {
             this.playerInventory.put(material, 0);
         }
+        this.coord = new Coordinate(0, 0);
     }
     
     @Override
     public void moveUp() {
-        this.coord = new Coordinate(this.coord.row()-1, this.coord.column());
+        if(this.coord.row() >= 1) {
+            this.coord = new Coordinate(this.coord.row()-1, this.coord.column());
+        }
     }
 
     @Override
     public void moveRight() {
-        this.coord = new Coordinate(this.coord.row(), this.coord.column()+1);
+        if(this.coord.column() < this.gameconfig.getLabyrinthWidth()-1) {
+            this.coord = new Coordinate(this.coord.row(), this.coord.column()+1);
+        }
     }
 
     @Override
     public void moveLeft() {
-        this.coord = new Coordinate(this.coord.row(), this.coord.column()-1);
+        if(this.coord.column() >= 1) {
+            this.coord = new Coordinate(this.coord.row(), this.coord.column()-1);
+        }
     }
 
     @Override
     public void moveDown() {
-        this.coord = new Coordinate(this.coord.row()+1, this.coord.column());
+        if(this.coord.row() < this.gameconfig.getLabyrinthHeight()-1) {
+            this.coord = new Coordinate(this.coord.row()+1, this.coord.column());
+        }
     }
 
     @Override
@@ -46,8 +57,10 @@ public class PlayerImpl implements Player{
 
     @Override
     public void decreaseQuantityMaterial(final Material material, final int amount) {
-        int newValue = this.getQuantityMaterial(material) - amount;
-        this.playerInventory.replace(material, newValue);
+        if(this.playerInventory.get(material) >= amount) {
+            int newValue = this.getQuantityMaterial(material) - amount;
+            this.playerInventory.replace(material, newValue);
+        }
     }
 
     @Override

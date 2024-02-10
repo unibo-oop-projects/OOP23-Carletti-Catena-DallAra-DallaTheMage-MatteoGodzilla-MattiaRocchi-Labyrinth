5184820417ab27d8173a5f1676eb9490b62ed3bc
Lorenzo Carletti <lorenzo.carletti3@studio.unibo.it>
2024-02-10 15:19:03 +0100

    Fixed checkstyle for PlayerTest

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -10,16 +10,26 @@ import com.ccdr.labyrinth.game.loader.Coordinate;
 import com.ccdr.labyrinth.game.player.Player;
 import com.ccdr.labyrinth.game.player.PlayerImpl;
 
-public class PlayerTest {
-    
+/**
+ * A test class for the implementation of a player.
+ * It's final because the class is not designed for extension.
+ */
+public final class PlayerTest {
+
     private Player player;
     private GameConfig gameConfig = new GameConfig();
 
+    /**
+     * Method to initialize the field's player.
+     */
     @BeforeEach
     public void init() {
         this.player = new PlayerImpl();
     }
 
+    /**
+     * Test the functioning of the moveUp() method.
+     */
     @Test
     public void testMoveUp() {
         var player1 = this.player;
@@ -29,19 +39,25 @@ public class PlayerTest {
         assertEquals(new Coordinate(0, 0), player1.getCoord());
     }
 
+    /**
+     * Test the functioning of the moveRight() method.
+     */
     @Test
     public void testMoveRight() {
         var player1 = this.player;
         assertEquals(new Coordinate(0, 0), player1.getCoord());
         player1.moveRight();
         assertEquals(new Coordinate(0, 1), player1.getCoord());
-        for(int i = 0; i < this.gameConfig.getLabyrinthWidth(); i++) {
+        for (int i = 0; i < this.gameConfig.getLabyrinthWidth(); i++) {
             player1.moveRight();
         }
-        assertNotEquals(new Coordinate(0, 30), player1.getCoord());
-        assertEquals(new Coordinate(0, 29), player1.getCoord());
+        assertNotEquals(new Coordinate(0, this.gameConfig.getLabyrinthWidth()), player1.getCoord());
+        assertEquals(new Coordinate(0, this.gameConfig.getLabyrinthWidth() - 1), player1.getCoord());
     }
 
+    /**
+     * Test the functioning of the moveLeft() method.
+     */
     @Test
     public void testMoveLeft() {
         var player1 = this.player;
@@ -51,41 +67,50 @@ public class PlayerTest {
         assertEquals(new Coordinate(0, 0), player1.getCoord());
     }
 
+    /**
+     * Test the functioning of the moveDown() method.
+     */
     @Test
     public void testMoveDown() {
         var player1 = this.player;
         assertEquals(new Coordinate(0, 0), player1.getCoord());
         player1.moveDown();
         assertEquals(new Coordinate(1, 0), player1.getCoord());
-        for(int i = 0; i < this.gameConfig.getLabyrinthHeight(); i++) {
+        for (int i = 0; i < this.gameConfig.getLabyrinthHeight(); i++) {
             player1.moveDown();
         }
-        assertNotEquals(new Coordinate(30, 0), player1.getCoord());
-        assertEquals(new Coordinate(29, 0), player1.getCoord());
+        assertNotEquals(new Coordinate(this.gameConfig.getLabyrinthHeight(), 0), player1.getCoord());
+        assertEquals(new Coordinate(this.gameConfig.getLabyrinthHeight() - 1, 0), player1.getCoord());
     }
 
+    /**
+     * Test the points management methods.
+     */
     @Test
     public void testPoints() {
         var player1 = this.player;
         assertEquals(0, player1.getPoints());
-        player1.increasePoints(10);
-        assertEquals(10, player1.getPoints());
-        player1.increasePoints(6);
-        assertEquals(16, player1.getPoints());
+        player1.increasePoints(4);
+        assertEquals(4, player1.getPoints());
+        player1.increasePoints(4);
+        assertEquals(8, player1.getPoints());
     }
 
+    /**
+     * Test the inventory management methods.
+     */
     @Test
     public void testInventory() {
         var player1 = this.player;
         assertEquals(0, player1.getQuantityMaterial(Material.WOOD));
         assertEquals(0, player1.getQuantityMaterial(Material.IRON));
-        player1.increaseQuantityMaterial(Material.WOOD, 5);
-        assertEquals(5, player1.getQuantityMaterial(Material.WOOD));
-        player1.increaseQuantityMaterial(Material.IRON, 18);
-        assertEquals(18, player1.getQuantityMaterial(Material.IRON));
-        player1.decreaseQuantityMaterial(Material.WOOD, 10);
-        assertEquals(5, player1.getQuantityMaterial(Material.WOOD));
+        player1.increaseQuantityMaterial(Material.WOOD, 10);
+        assertEquals(10, player1.getQuantityMaterial(Material.WOOD));
+        player1.increaseQuantityMaterial(Material.IRON, 4);
+        assertEquals(4, player1.getQuantityMaterial(Material.IRON));
+        player1.decreaseQuantityMaterial(Material.WOOD, 8);
+        assertEquals(2, player1.getQuantityMaterial(Material.WOOD));
         player1.decreaseQuantityMaterial(Material.IRON, 3);
-        assertEquals(15, player1.getQuantityMaterial(Material.IRON));
+        assertEquals(1, player1.getQuantityMaterial(Material.IRON));
     }
 }

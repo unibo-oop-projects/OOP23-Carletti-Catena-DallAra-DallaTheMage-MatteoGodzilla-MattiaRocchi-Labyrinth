c8e608cae1a2a1cf85861dd96b1e5fd9ce0146fc
Matteo Catena <matteo.catena3@studio.unibo.it>
2024-02-10 14:24:49 +0100

    Merged BonusTile class into StandardTile, since they were basically the same

--- src/main/java/com/ccdr/labyrinth/game/loader/tiles/StandardTile.java
+++ src/main/java/com/ccdr/labyrinth/game/loader/tiles/StandardTile.java
@@ -1,22 +1,33 @@
 package com.ccdr.labyrinth.game.loader.tiles;
 
+import java.util.Optional;
+
+import com.ccdr.labyrinth.Material;
 import com.ccdr.labyrinth.game.player.Player;
 
 public class StandardTile extends GenericTile {
+    private Optional<Material> bonusMaterial = Optional.empty();
+    private int bonusAmount;
+
     public StandardTile() {
         super.setType(TileType.NORMAL);
     }
 
-    /*TODO: OPTIONAL STANDARDTILE BONUS MATERIALS*/
-    @Override
-    public void onEnter(final Player player) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onEnter'");
+    public StandardTile(Material bonusMaterial, int amount) {
+        super.setType(TileType.BONUS);
+        this.bonusMaterial = Optional.of(bonusMaterial);
+        this.bonusAmount = amount;
     }
 
     @Override
-    public void onExit(final Player player) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onExit'");
+    public void onEnter(final Player player) {
+        if (this.bonusMaterial.isPresent()) {
+            player.increaseQuantityMaterial(this.bonusMaterial.get(), this.bonusAmount);
+            this.bonusMaterial = Optional.empty();
+            this.bonusAmount = 0;
+        }
     }
+
+    @Override
+    public void onExit(final Player player) { }
 }

ba4856e1a28f95552fcf8baccec17b2a9a7edd44
Lorenzo Carletti <lorenzo.carletti3@studio.unibo.it>
2024-02-10 10:54:58 +0100

    Fixed checkstyle for interface Player

--- src/main/java/com/ccdr/labyrinth/game/player/Player.java
+++ src/main/java/com/ccdr/labyrinth/game/player/Player.java
@@ -3,62 +3,66 @@ package com.ccdr.labyrinth.game.player;
 import com.ccdr.labyrinth.Material;
 import com.ccdr.labyrinth.game.loader.Coordinate;
 
+/**
+ * An interface modelling a player, that can move in the four directions, 
+ * and with an inventory to contains materials.
+ */
 public interface Player {
     /**
-     * Move the player of one tile up
+     * Move the player of one tile up.
      */
     void moveUp();
 
     /**
-     * Move the player one tile to the right
+     * Move the player one tile to the right.
      */
     void moveRight();
 
     /**
-     * Move the player one tile to the left
+     * Move the player one tile to the left.
      */
     void moveLeft();
 
     /**
-     * Move the player one tile down
+     * Move the player one tile down.
      */
     void moveDown();
 
     /**
-     * Increase the quantity of a specified material
+     * Increase the quantity of a specified material.
      * @param material the material to increase
      * @param amount the quantity to increase
      */
     void increaseQuantityMaterial(Material material, int amount);
 
     /**
-     * Decrease the quantity of a specified material
+     * Decrease the quantity of a specified material.
      * @param material the material to decrease
      * @param amount the quantity to decrease
      */
     void decreaseQuantityMaterial(Material material, int amount);
 
     /**
-     * Gives the quantity of a material in the inventory
+     * Gives the quantity of a material in the inventory.
      * @param material the specified material
      * @return the quantity of a specified material
      */
     int getQuantityMaterial(Material material);
 
     /**
-     * Increase the value of points
+     * Increase the value of points.
      * @param amount the value to increase points
      */
     void increasePoints(int amount);
 
     /**
-     * Gives the quantity of points
+     * Gives the quantity of points.
      * @return the points' value
      */
     int getPoints();
 
     /**
-     * Gives the coordinate of a player
+     * Gives the coordinate of a player.
      * @return coordinate of a player
      */
     Coordinate getCoord();

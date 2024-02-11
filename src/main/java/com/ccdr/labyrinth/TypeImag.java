6b829d0f7eb0f9be181cb85a23ff70249ccfb271
Matteo Catena <matteo.catena3@studio.unibo.it>
2024-02-11 17:34:50 +0100

    added textures for labyrinth tiles and materials

--- src/main/java/com/ccdr/labyrinth/TypeImag.java
+++ src/main/java/com/ccdr/labyrinth/TypeImag.java
@@ -8,11 +8,21 @@ public enum TypeImag {
     //path is redirected through src/main/resources
     //if the file could not be found, jfx will throw an exception
     //(it doesn't do that when you use the 'file:' protocol)
-    LOGO("/menu/Logo-v3.png");
-    //TEXT("/menu/Text.png");
-    //WALL("/Game/Wall.png"),
-    //PATHVERTICAL("/Game/PathVertical.png"),
-    //PATHHORIZONTAL("/Game/PathHorizontal.png");
+    //MENU
+    LOGO("/menu/Logo-v3.png"),
+    //GAME
+    WALL("/game/Wall.png"),
+    PATH_VERTICAL("/game/PathVertical.png"),
+    PATH_HORIZONTAL("/game/PathHorizontal.png"),
+    PATH_CENTRAL("/game/PathCentral.png"),
+    PATH("/game/Path.png"),
+    //MATERIALS
+    COAL("/game/materials/Coal.png"),
+    COPPER("/game/materials/Copper.png"),
+    DIAMOND("/game/materials/Diamond.png"),
+    IRON("/game/materials/Iron.png"),
+    SILK("/game/materials/Silk.png"),
+    WOOD("/game/materials/Wood.png");
 
     private String path;
     private Image picture;

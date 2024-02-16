0ed28408ec1ab3f1efadb4b3347dfc5afcc1ee7d
MattiaRocchi <mattia.rocchi2@studio.unibo.it>
2024-02-16 16:18:16 +0100

    TestGuild first implementation

--- src/test/java/labyrinth/GuildTileTest.java
+++ src/test/java/labyrinth/GuildTileTest.java
@@ -2,28 +2,33 @@ package labyrinth;
 
 import org.junit.jupiter.api.Test;
 
+<<<<<<< Updated upstream
 import com.ccdr.labyrinth.game.tiles.GuildTile;
 import com.ccdr.labyrinth.game.util.Item;
 import com.ccdr.labyrinth.game.util.Material;
 
+import com.ccdr.labyrinth.game.context.GuildContext;
+import com.ccdr.labyrinth.TypeImag;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
-
 class GuildTileTest {
-    /**
+    
+    GuildContext guildContext = new GuildContext(2);
+
     @Test
-    /**void testGetMissions() {
-        final GuildTile guild = new GuildTile(4, 10);
-        final List<Item> missions = guild.returnListOfMissions();
-        final List<Material> materials = guild.getMaterialPresents();
-        for (final Item item : missions) {
-
-            System.out.println("RES:" + item.getPoints() + item.getQuantity() + item.getMaterial() + item.getCategory());
+    void testMissions() {
+        final List<Item> missions = guildContext.getListOfMissions();
+        for (Item mission : missions) {
+            assertEquals(missions.get(guildContext.getMenuIndex()).getMaterial(), guildContext.getListOfMissions().get(guildContext.getMenuIndex()).getMaterial());
+            guildContext.down();
         }
 
-        for (final Material mat : materials) {
-            System.out.println(mat);
-        }
+    }
 
-
-    }*/
+    @Test
+    void testImage() {
+        
+    }
 }

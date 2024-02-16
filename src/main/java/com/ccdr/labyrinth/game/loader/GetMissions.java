7e12a68818ecdab6cbc1ca6594e5cfd7ab49594e
MattiaRocchi <mattia.rocchi2@studio.unibo.it>
2024-02-16 01:37:43 +0100

    Checkstyle fix in GetMissions class

--- src/main/java/com/ccdr/labyrinth/game/loader/GetMissions.java
+++ src/main/java/com/ccdr/labyrinth/game/loader/GetMissions.java
@@ -9,11 +9,13 @@ import java.util.HashSet;
 import com.ccdr.labyrinth.Category;
 import com.ccdr.labyrinth.Material;
 
-/*Class for creating missions belonging to the guild */
+/**
+ * Class for creating missions belonging to the guild.
+ */
 public class GetMissions {
-    private final int MIN_REQUIRED = 5;
-    private final int MAX_REQUIRED = 10;
-    private int material_temp = 0;
+    private final int minRequired = 5;
+    private final int maxRequired = 10;
+    private int materialtemp = 0;
     private int idCat = 0;
     private int idMat = 0;
     private Set<Category> category = new HashSet<>(Set.of(Category.values()));
@@ -21,55 +23,64 @@ public class GetMissions {
     private Set<Material> material = new HashSet<>(Set.of(Material.values()));
     private Random quantityGenerator = new Random();
 
-    /*method for generating a mission considering that there must not be identical missions
-      and that there must be two missions per material */
-    public Item generateMission() {
-        Item item = new Item(); /*creation of the new Item to be able to generate a list */
+    /**
+     * method for generating a mission considering that there must not be 
+     * identical missions and that there must be two missions per material.
+     * @return one item tha is a mission
+     */
+    public final Item generateMission() {
+        Item item = new Item(); /**creation of the new Item to be able to generate a list. */
 
-        /*Condition used to not repeat the same category with the same material */
-        if (material_temp == 1 || material_temp == 0) {
-            if (material_temp == 1) {
+        /**Condition used to not repeat the same category with the same material. */
+        if (materialtemp == 1 || materialtemp == 0) {
+            if (materialtemp == 1) {
                 category.remove(Category.values()[idCat]);
             }
 
             do {
-                idCat = quantityGenerator.nextInt(0,5);
-            } while(!category.contains(Category.values()[idCat]));
+                idCat = quantityGenerator.nextInt(0, 2 * 2 + 1);
+            } while (!category.contains(Category.values()[idCat]));
         }
 
-        /*Condition used to not repeat the same material */
-        if (material_temp == 2 || material_temp == 0) {
-            if (material_temp == 2) {
+        /**Condition used to not repeat the same material. */
+        if (materialtemp == 2 || materialtemp == 0) {
+            if (materialtemp == 2) {
                 material.remove(Material.values()[idMat]);
-                material_temp = 0;
+                materialtemp = 0;
             }
 
             do {
-                idMat = quantityGenerator.nextInt(0,6);
-            } while(!material.contains(Material.values()[idMat]));
+                idMat = quantityGenerator.nextInt(0, 2 * 3);
+            } while (!material.contains(Material.values()[idMat]));
         }
 
         item.setCategory(Category.values()[idCat]);
         item.setMaterial(Material.values()[idMat]);
-        material_temp++;
+        materialtemp++;
 
         if (!materialpresents.contains(item.getMaterial())) {
             materialpresents.add(item.getMaterial());
         }
-        /*Set quantity of materials */
-        item.setQuantity(quantityGenerator.nextInt(MIN_REQUIRED,MAX_REQUIRED));
-        /*Set quantity of points */
-        item.setPoints(quantityGenerator.nextInt(6,11));
+        /**Set quantity of materials. */
+        item.setQuantity(quantityGenerator.nextInt(minRequired, maxRequired));
+        /**Set quantity of points. */
+        item.setPoints(quantityGenerator.nextInt(2 * 3, 10 + 1));
 
         return item;
 
     }
-    
-    public int getMaxPoints(){
-        return this.MAX_REQUIRED;
+    /**
+     * 
+     * @return max point
+     */
+    public final int getMaxPoints() {
+        return this.maxRequired;
     }
-
-    public List<Material> materialPresents() {
+    /**
+     * 
+     * @return materials presents in game
+     */
+    public final List<Material> materialPresents() {
         return materialpresents;
     }
 }

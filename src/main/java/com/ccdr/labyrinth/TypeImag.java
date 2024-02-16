5b8cc594c80f73c8d58dd3df15d85cf130801d0f
MattiaRocchi <mattia.rocchi2@studio.unibo.it>
2024-02-16 01:54:40 +0100

    Checkstyle fix in TypeImag class

--- src/main/java/com/ccdr/labyrinth/TypeImag.java
+++ src/main/java/com/ccdr/labyrinth/TypeImag.java
@@ -1,41 +1,103 @@
 package com.ccdr.labyrinth;
 
 import javafx.scene.image.Image;
-
+/**
+ * Enum for loading from file path and getting images.
+ */
 public enum TypeImag {
-    //path is redirected through src/main/resources
-    //if the file could not be found, jfx will throw an exception
-    //(it doesn't do that when you use the 'file:' protocol)
-    //MENU
+    /**path is redirected through src/main/resources.
+    *if the file could not be found, jfx will throw an exception
+    *(it doesn't do that when you use the 'file:' protocol)
+    */
+    /**
+     * Menu.
+     */
     LOGO("/menu/Logo-v3.png"),
-    //GAME
+    /**
+     * Game.
+     */
     WALL("/game/Wall.png"),
+    /**
+     * 
+     */
     PATH_VERTICAL("/game/PathVertical.png"),
+    /**
+     * 
+     */
     PATH_HORIZONTAL("/game/PathHorizontal.png"),
+    /**
+     * 
+     */
     PATH_CENTRAL("/game/PathCentral.png"),
+    /**
+     * 
+     */
     PATH("/game/Path.png"),
+    /**
+     * 
+     */
     GUILD("/game/Guild.png"),
+    /**
+     * 
+     */
     POINT("/game/Point.png"),
-    //MATERIALS
+    /**
+     * Materials.
+     */
     COAL("/game/materials/Coal.png"),
+    /**
+     * 
+     */
     COPPER("/game/materials/Copper.png"),
+    /**
+     * 
+     */
     DIAMOND("/game/materials/Diamond.png"),
+    /**
+     * 
+     */
     IRON("/game/materials/Iron.png"),
+    /**
+     * 
+     */
     SILK("/game/materials/Silk.png"),
+    /**
+     * 
+     */
     WOOD("/game/materials/Wood.png"),
-    //CATEGORYS
+    /**
+     * Categorys.
+     */
     ARMOR("/game/category/Armor.png"),
+    /**
+     * 
+     */
     CLOTHING("/game/category/Clothing.png"),
+    /**
+     * 
+     */
     JEWEL("/game/category/Jewel.png"),
+    /**
+     * 
+     */
     TOOL("/game/category/Tool.png"),
+    /**
+     * 
+     */
     WEAPON("/game/category/Weapon.png");
 
     private Image picture;
-
-    private TypeImag(final String path) {
+    /**
+     * 
+     * @param path
+     */
+    TypeImag(final String path) {
         this.picture = new Image(path, 0, 0, true, true);
     }
-
+    /**
+     * 
+     * @return image
+     */
     public Image getImage() {
         return picture;
     }
